7ca9d26865a32d3dded19d0ede376e51b6153709
EmilSkjetne <emil.skjetne@gmail.com>
2021-04-21 09:56:15 +0200

    Begynnte me JS

--- Praksis Hjemmeside/javascript.js
+++ Praksis Hjemmeside/javascript.js
@@ -1,1 +1,32 @@
-var idk = idk
+// tok koden fra https://thoughtbot.com/blog/pong-clone-in-javascript
+
+//er meget ny til javascript så fikk koden fra en nettside som forklarte det.
+
+//kommer til og leke litt med denne koden, men kommer ikke til og pushe det
+
+var aminate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) {window.setTimeout(callback, 1000/60)}
+
+    var canvas = document.createElement;
+    var width = 400;
+    var height = 400;
+    canvas.width = width;
+    canvas.height = height;
+
+    window.onload = function() {
+        document.body.appendChild(canvas);
+        animate(step);
+      };
+
+        var step(){
+            update();
+            render();
+            animate(step);
+        }
+
+
+        var update = function(){
+
+        }
