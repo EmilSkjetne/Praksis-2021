ac5d07ba5de1bad066993c22a0670ae5902385cd
EmilSkjetne <emil.skjetne@gmail.com>
2021-04-22 12:55:01 +0200

    fikset ballen, jobber med kollisjon

--- Praksis Hjemmeside/javascript.js
+++ Praksis Hjemmeside/javascript.js
@@ -29,20 +29,19 @@ var render = function(){
 }          
 
 var step = function(){
-    //update();
+    update();
     render();
-    console.log("hola");
     animate(step);
     }
     
 window.onload = function() {
-    console.log(canvas);
     document.body.appendChild(canvas);
     animate(step);
     };
 
 
 var update = function(){
+    ball.update();
 }
 
 function Paddle(x, y, width, height){
@@ -61,7 +60,7 @@ Paddle.prototype.render = function(){
 
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(150, 150);
+var ball = new Ball(150, 150, 10);
 
 function Player(){
     this.paddle = new Paddle(175, 380, 50, 10);
@@ -79,25 +78,70 @@ Computer.prototype.render = function() {
     this.paddle.render();
 };
 
-function Ball(x, y){
-    this.radius = 10;
+function Ball(x, y, r){
     this.x = x;
     this.y = y;
-    this.x_speed = 0;
+    this.radius = r;
+    this.x_speed = -1;
     this.y_speed = 1;
 }
 
 Ball.prototype.render = function(){
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 2 * Math.PI, false)
+    context.stroke();
     context.fillStyle = "#000000";
+
 }
 
 var render = function(){
-    console.log("render 2")
     context.fillStyle = "rgba(255, 255, 255, 0.5)";
     context.fillRect(0, 0, width, height);
     player.render();
     computer.render();
     ball.render();
 }   
+
+Ball.prototype.update = function(){
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    console.log("moved")
+}
+
+var update = function(){
+    ball.update(player.paddle, computer.paddle);
+}
+
+Ball.prototype.update = function(paddle1, paddle2){
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.x - 5;
+    var top_y = this.y - 5;
+    var bottom_x = this.x +5;
+    var bottom_y = this.y + 5;
+}
+
+if (this.x -5 <0){
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+}
+
+else if (this.x + 5 > 400){
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+}
+
+//scoring a point
+
+if (this.y < 0 || this.y > 400) {
+    this.x = 200
+    this.y = 300
+    this.x_speed = 0;
+    this.y_speed = 3;
+}
+
+if (top_y > 300) {
+    if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x){
+
+    }
+}
