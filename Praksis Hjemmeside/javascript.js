a735d0d9c2086e1fbb1b9f0bf1dad979fa3ce78d
EmilSkjetne <emil.skjetne@gmail.com>
2021-04-23 10:32:44 +0200

    ferdig, mangler bare noen kommentarer

--- Praksis Hjemmeside/javascript.js
+++ Praksis Hjemmeside/javascript.js
@@ -4,16 +4,14 @@
 
 //kommer til og leke litt med denne koden, men kommer ikke til og pushe det
 
+//gjør så vi har 60 bilder i sekundet
 
-//var animate = window.requestAnimationFrame ||
-  //  window.webkitRequestAnimationFrame ||
-    //window.mozRequestAnimationFrame ||
-    //function(callback) {window.setTimeout(callback, 1000/60)}
+var animate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) {window.setTimeout(callback, 1000/60)}
 
-var animate = function(callback) {
-    window.setTimeout(callback, 1000/60)
-}
-
+// variabler til canvas størrelse
 
 var canvas = document.createElement('canvas');
 var width = 400;
@@ -22,11 +20,15 @@ canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 
+//gir context til canvas så den vet hvordan den skal se ut
+
 var render = function(){
     console.log("render");
     context.fillStyle = "rgba(255, 255, 255, 0.5)";
     context.fillRect(0, 0, width, height);
 }          
+
+// Loopen som kjører spillet
 
 var step = function(){
     update();
@@ -34,15 +36,20 @@ var step = function(){
     animate(step);
     }
     
+//Når vinduet blir lastet kjører dette og lager et canvas element. Starter også loopen
+
 window.onload = function() {
     document.body.appendChild(canvas);
     animate(step);
     };
 
+//Funksjonen som oppdaterer ballen
 
 var update = function(){
     ball.update();
 }
+
+//En mal for hvordan en paddle funksjon skal se ut. Man setter in x, y, width og height så får man en paddle.
 
 function Paddle(x, y, width, height){
     this.x = x;
@@ -53,30 +60,44 @@ function Paddle(x, y, width, height){
     this.y_speed = 0;
 }
 
+// rendrer paddlenen med det som ble spesifisert i paddle funsjonen
+
 Paddle.prototype.render = function(){
     context.fillStyle = "#0000FF";
     context.fillRect(this.x, this.y, this.width, this.height);
 }
 
+//bruker variablene til og lage nye paddeler og en ball
+
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball(150, 150, 10);
+
+// lager en paddel med malen vi så på tiligere
 
 function Player(){
     this.paddle = new Paddle(175, 380, 50, 10);
 }
 
+//lager en paddel med malen vi så på tiligere
+
 function Computer(){
     this.paddle = new Paddle(175, 10, 50, 10);
 }
+
+//rendrer paddelen som ble lagdt tiligere av malen
 
 Player.prototype.render = function() {
     this.paddle.render();
   };
   
+//rendrer paddelen som be lagdt tiligere av malen
+
 Computer.prototype.render = function() {
     this.paddle.render();
 };
+
+//har alle variablene til ballen
 
 function Ball(x, y, r){
     this.x = x;
@@ -86,6 +107,8 @@ function Ball(x, y, r){
     this.y_speed = 1;
 }
 
+//rendrer ballen
+
 Ball.prototype.render = function(){
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 2 * Math.PI, false)
@@ -93,6 +116,8 @@ Ball.prototype.render = function(){
     context.fillStyle = "#000000";
 
 }
+
+//Rendrer det som ble kallt på tiligere
 
 var render = function(){
     context.fillStyle = "rgba(255, 255, 255, 0.5)";
@@ -102,22 +127,28 @@ var render = function(){
     ball.render();
 }   
 
+//Rendrer ballen og gir den en spessiell fart
+
 Ball.prototype.update = function(){
     this.x += this.x_speed;
     this.y += this.y_speed;
     console.log("moved")
 }
 
+//Oppdaterer ballen og paddelene
+
 var update = function(){
     ball.update(player.paddle, computer.paddle);
 }
+
+
 
 Ball.prototype.update = function(paddle1, paddle2){
         this.x += this.x_speed;
         this.y += this.y_speed;
         var top_x = this.x - 5;
         var top_y = this.y - 5;
-        var bottom_x = this.x +5;
+        var bottom_x = this.x + 5;
         var bottom_y = this.y + 5;
     
 
@@ -135,7 +166,7 @@ Ball.prototype.update = function(paddle1, paddle2){
 
     if (this.y < 0 || this.y > 400) {
         this.x = 200
-        this.y = 300
+        this.y = 200
         this.x_speed = 0;
         this.y_speed = 3;
     }
@@ -143,7 +174,7 @@ Ball.prototype.update = function(paddle1, paddle2){
     if (top_y > 300) {
         if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x){
             //hit the player paddle
-            this.y_speed = 3;
+            this.y_speed = -3;
             this.x_speed += (paddle1.x_speed / 2)
             this.y += this.y_speed
         }
@@ -158,3 +189,67 @@ Ball.prototype.update = function(paddle1, paddle2){
         }
       }
     };
+
+var keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+    keysDown[event.keyCode] = true;
+});
+    
+window.addEventListener("keyup", function(event) {
+    delete keysDown[event.keyCode];
+});
+
+var update = function() {
+    player.update();
+    ball.update(player.paddle, computer.paddle);
+  };
+  
+Player.prototype.update = function() {
+    for(var key in keysDown) {
+        var value = Number(key);
+        if(value == 37) { // left arrow
+            this.paddle.move(-4, 0);
+        } else if (value == 39) { // right arrow
+            this.paddle.move(4, 0);
+        } else {
+            this.paddle.move(0, 0);
+    }
+}
+};
+  
+Paddle.prototype.move = function(x, y) {
+    this.x += x;
+    this.y += y;
+    this.x_speed = x;
+    this.y_speed = y;
+    if(this.x < 0) { // all the way to the left
+      this.x = 0;
+      this.x_speed = 0;
+    } else if (this.x + this.width > 400) { // all the way to the right
+      this.x = 400 - this.width;
+      this.x_speed = 0;
+    }
+  }
+
+var update = function() {
+    player.update();
+    computer.update(ball);
+    ball.update(player.paddle, computer.paddle);
+  };
+  
+Computer.prototype.update = function(ball) {
+    var x_pos = ball.x;
+    var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
+    if(diff < 0 && diff < -4) { // max speed left
+      diff = -5;
+    } else if(diff > 0 && diff > 4) { // max speed right
+      diff = 5;
+    }
+    this.paddle.move(diff, 0);
+    if(this.paddle.x < 0) {
+      this.paddle.x = 0;
+    } else if (this.paddle.x + this.paddle.width > 400) {
+      this.paddle.x = 400 - this.paddle.width;
+    }
+  };
